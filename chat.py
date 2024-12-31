3398a53c440d2f91a2e974347c96126307d50e69
Hamza Qureshi - Desktop <hamza7771.861@gmail.com>
2025-01-01 00:34:17 +0500

    Signed-off-by: Hamza Qureshi - Desktop <hamza7771.861@gmail.com>

--- chat.py
+++ chat.py
@@ -133,3 +133,12 @@ class Bot:
         answer = result["answer"]
         print(answer)
         return answer
+
+
+# from chat import Bot
+
+bot = Bot()
+bot.load_PDF_doc("documents/temp.pdf")
+bot.retreval()
+query = "what is this document?"
+bot.chat(query)
--- main.py
+++
@@ -1,7 +0,0 @@
-from chat import Bot
-
-bot = Bot()
-bot.load_PDF_doc("documents/temp.pdf")
-bot.retreval()
-query = "what is this document?"
-bot.chat(query)
